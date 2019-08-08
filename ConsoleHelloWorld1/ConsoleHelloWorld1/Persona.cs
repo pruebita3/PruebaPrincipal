1bc0e12f545ba624db27c9cdb3e119a4f1b5a3b9
Ivonne Moreno <ivon.mb25@gmail.com>
2019-08-07 21:40:33 -0500

    se agrego quiero comer

--- ConsoleHelloWorld1/ConsoleHelloWorld1/Persona.cs
+++ ConsoleHelloWorld1/ConsoleHelloWorld1/Persona.cs
@@ -7,6 +7,7 @@ namespace ConsoleHelloWorld1
     class Persona
     {
         public int MyProperty { get; set; }
-        string mensaje = "hola";
+        string mensaje = "hola mundo";
+        string mensaje1 = "quiero comer";
     }
 }
